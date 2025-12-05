9d97d8a53d7954ebe96e5d481fd33f7671a2dc68
Pranav <pranavjha0002@gmail.com>
2025-12-05 16:34:07 +0530

    Export chain-related modules from index.js

--- chains/index.js
+++ chains/index.js
@@ -1,1 +1,12 @@
+import * as chainIds from "./chainIds.js";
+import { RPC_URLS, getRpcUrl } from "./rpc.js";
+import { NETWORKS, isMainnet, isTestnet } from "./networks.js";
 
+export {
+  chainIds,
+  RPC_URLS,
+  getRpcUrl,
+  NETWORKS,
+  isMainnet,
+  isTestnet,
+};
