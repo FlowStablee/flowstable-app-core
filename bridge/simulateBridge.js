c91eb0ddbadad63bfcb16b81cae038c1dbf4b39e
Pranav <pranavjha0002@gmail.com>
2025-12-06 15:59:46 +0530

    Update simulateBridge.js

--- bridge/simulateBridge.js
+++ bridge/simulateBridge.js
@@ -1,1 +1,12 @@
+export function simulateBridge(fromChain, toChain, amount) {
+  const fee = amount * 0.001; // Placeholder fee calc
+
+  return {
+    fromChain,
+    toChain,
+    amount,
+    estimatedFee: fee,
+    estimatedReceived: amount - fee
+  };
+}
 
