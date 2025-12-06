2e4b918ef4d98b4a9a5a2d4f250356fdab9632c2
Pranav <pranavjha0002@gmail.com>
2025-12-06 15:55:08 +0530

    Update unwrapUSDT0.js

--- unwrap/unwrapUSDT0.js
+++ unwrap/unwrapUSDT0.js
@@ -1,1 +1,28 @@
+import { ethers } from "ethers";
+import { getRpcEndpoint } from "../chains/rpcendpoints.js";
+
+/*
+  unwrapUSDT0(wallet, chainId, contractAddress, amount)
+
+  amount = number (in human readable form, e.g. "10")
+*/
+
+export async function unwrapUSDT0(wallet, chainId, contractAddress, amount) {
+  const rpc = getRpcEndpoint(chainId);
+  const provider = new ethers.JsonRpcProvider(rpc);
+  const signer = wallet.connect(provider);
+
+  // Temporary ABI until you give real contract ABI
+  const abi = [
+    "function unwrap(uint256 amount) public returns (bool)"
+  ];
+
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+
+  // Convert amount to wei (18 decimals assumed, update later)
+  const amountWei = ethers.parseEther(String(amount));
+
+  const tx = await contract.unwrap(amountWei);
+  return tx;
+}
 
