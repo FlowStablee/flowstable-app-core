8e6fcac09640fa43444427311b89eeb2e557552e
Pranav <pranavjha0002@gmail.com>
2025-12-06 15:59:02 +0530

    Update format.js

--- utils/format.js
+++ utils/format.js
@@ -1,1 +1,23 @@
+import { ethers } from "ethers";
+
+export const formatAddress = (address) => {
+  if (!address) return "";
+  return address.slice(0, 6) + "..." + address.slice(-4);
+};
+
+export const formatEth = (value) => {
+  try {
+    return Number(ethers.formatEther(value)).toFixed(6);
+  } catch {
+    return "0.000000";
+  }
+};
+
+export const parseEth = (value) => {
+  try {
+    return ethers.parseEther(String(value));
+  } catch {
+    throw new Error("Invalid amount");
+  }
+};
 
