1ee37adf0c11ac7496538bd3ddd7ccfe8166c2f8
Pranav <pranavjha0002@gmail.com>
2025-12-06 15:53:48 +0530

    Update importWallet.js

--- wallet/importWallet.js
+++ wallet/importWallet.js
@@ -1,1 +1,10 @@
+import { ethers } from "ethers";
+
+export function importWallet(privateKey) {
+  try {
+    return new ethers.Wallet(privateKey);
+  } catch {
+    throw new Error("Invalid private key");
+  }
+}
 
