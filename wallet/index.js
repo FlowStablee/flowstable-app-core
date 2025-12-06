85171113701a73f576dbc1f98904f58c3a5570ef
Pranav <pranavjha0002@gmail.com>
2025-12-06 15:51:47 +0530

    Add wallet functions for creation and transactions

--- wallet/index.js
+++ wallet/index.js
@@ -1,1 +1,30 @@
+import { ethers } from "ethers";
+import { getRpcEndpoint } from "../chains/rpcendpoints.js";
+
+export const createWallet = () => {
+  return ethers.Wallet.createRandom();
+};
+
+export const importWallet = (privateKey) => {
+  try {
+    return new ethers.Wallet(privateKey);
+  } catch (err) {
+    throw new Error("Invalid private key");
+  }
+};
+
+export const getSigner = (wallet, chainId) => {
+  const rpc = getRpcEndpoint(chainId);
+  const provider = new ethers.JsonRpcProvider(rpc);
+  return wallet.connect(provider);
+};
+
+export const sendNative = async (wallet, chainId, to, amountEth) => {
+  const signer = getSigner(wallet, chainId);
+  const tx = await signer.sendTransaction({
+    to,
+    value: ethers.parseEther(amountEth),
+  });
+  return tx;
+};
 
