8f41cd29e75e5af3cd4ae1f2040003ff4fd98741
Pranav <pranavjha0002@gmail.com>
2025-12-06 16:00:05 +0530

    Update sendBridgeTx.js

--- bridge/sendBridgeTx.js
+++ bridge/sendBridgeTx.js
@@ -1,1 +1,10 @@
+export async function bridgeTokens(wallet, fromChain, toChain, amount) {
+  return {
+    status: "queued",
+    message: "Bridge request submitted to Flowstable Router (placeholder)",
+    fromChain,
+    toChain,
+    amount
+  };
+}
 
